44746843bbe4fd8d6d2641cf9d6c9dc4d0963005
MindOfZak <100719443@unimail.derby.ac.uk>
2026-03-17 11:51:49 +0000

    Started B06_B07

---
+++ proj02/src/main.cpp
@@ -0,0 +1,256 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/type_ptr.hpp> 
+
+#include "shader.h"
+#include "Mesh.h"
+#include "Node.h"
+
+static Shader shader;
+
+glm::mat4 matModelRoot = glm::mat4(1.0);
+glm::mat4 matView = glm::mat4(1.0);
+glm::mat4 matProj = glm::ortho(-2.0f,2.0f,-2.0f,2.0f, -2.0f,2.0f);
+
+glm::vec3 scale = glm::vec3(1.0, 1.0, 1.0);
+
+// camera settings
+glm::vec3 lightPos = glm::vec3(200.0f, 300.0f, 200.0f);
+glm::vec3 viewPos = glm::vec3(0.0f, 100.0f, 200.0f);
+glm::vec3 viewCenter = glm::vec3(0.0f, 100.0f, 0.0f);
+int wView = 800;
+int hView = 800;
+float fov = 70.0;
+float near = 0.1;
+float far = 400.0;
+
+
+GLuint blinnShader;
+GLuint texblinnShader;
+GLuint normalblinnShader;
+// GLuint boneShader;
+
+// Initialize shader
+GLuint initShader(std::string pathVert, std::string pathFrag) 
+{
+    shader.read_source( pathVert.c_str(), pathFrag.c_str());
+
+    shader.compile();
+    glUseProgram(shader.program);
+
+    return shader.program;
+}
+
+void setLightPosition(glm::vec3 lightPos)
+{
+    GLuint lightpos_loc = glGetUniformLocation(shader.program, "lightPos" );
+    glUniform3fv(lightpos_loc, 1, glm::value_ptr(lightPos));
+}
+
+void setViewPosition(glm::vec3 eyePos)
+{
+    GLuint viewpos_loc = glGetUniformLocation(shader.program, "viewPos" );
+    glUniform3fv(viewpos_loc, 1, glm::value_ptr(eyePos));
+}
+
+void window_size_callback(GLFWwindow* window, int w, int h)
+{
+    //int width, height;
+    //glfwGetWindowSize(window, &width, &height);
+
+    wView = w;
+    hView = h;
+
+    glViewport(0, 0, w, h);
+
+    matProj = glm::perspective(glm::radians(fov), w /(float) h, near, far);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    glm::mat4 mat = glm::mat4(1.0);
+
+    float angleStep = 5.0f;
+    float transStep = 1.0f;
+
+    if (action == GLFW_PRESS) {
+
+        if (mods & GLFW_MOD_CONTROL) {
+
+            // translation in world space
+            if (GLFW_KEY_LEFT == key) {
+                mat = glm::translate(glm::mat4(1.0f), glm::vec3(transStep, 0.0f, 0.0f));
+                matModelRoot = mat * matModelRoot;
+            } else if (GLFW_KEY_RIGHT == key ) {
+                mat = glm::translate(glm::mat4(1.0f), glm::vec3(-transStep, 0.0f, 0.0f));
+                matModelRoot = mat * matModelRoot;
+            } else if (GLFW_KEY_UP == key) {
+                mat = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, transStep, 0.0f));
+                matModelRoot = mat * matModelRoot;
+            } if (GLFW_KEY_DOWN == key) {
+                mat = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -transStep, 0.0f));
+                matModelRoot = mat * matModelRoot;
+            }
+        }
+
+        // camera control
+        if (GLFW_KEY_LEFT == key) {
+            // pan left, rotate around Y, CCW
+            mat = glm::rotate(glm::radians(-angleStep), glm::vec3(0.0, 1.0, 0.0));
+            matView = mat * matView;
+        } else if (GLFW_KEY_RIGHT == key ) {
+            // pan right, rotate around Y, CW
+            mat = glm::rotate(glm::radians(angleStep), glm::vec3(0.0, 1.0, 0.0));
+            matView = mat * matView;
+        } else if (GLFW_KEY_UP == key) {
+            // tilt up, rotate around X, CCW
+            mat = glm::rotate(glm::radians(-angleStep), glm::vec3(1.0, 0.0, 0.0));
+            matView = mat * matView;
+        } if (GLFW_KEY_DOWN == key) {
+            // tilt down, rotate around X, CW
+            mat = glm::rotate(glm::radians(angleStep), glm::vec3(1.0, 0.0, 0.0));
+            matView = mat * matView;
+        } else if ( (GLFW_KEY_KP_ADD == key) || 
+            (GLFW_KEY_EQUAL == key) && (mods & GLFW_MOD_SHIFT) ) {
+            // std::cout << "+ pressed" << std::endl;
+            // zoom in, move along -Z
+            mat = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, transStep));
+            matView = mat * matView ;
+        } else if ( (GLFW_KEY_KP_SUBTRACT == key ) || (GLFW_KEY_MINUS == key) ) {
+            // std::cout << "keypad - pressed" << std::endl;
+            // zoom out, move along -Z
+            mat = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -transStep));
+            matView = mat * matView;
+        } else if (GLFW_KEY_R == key) {
+            //std::cout << "R pressed" << std::endl;
+            // reset
+            matView = glm::lookAt(viewPos, viewCenter, glm::vec3(0, 1, 0)); 
+            matModelRoot = glm::mat4(1.0f);
+        }
+
+        // translation along camera axis (first person view)
+        else if (GLFW_KEY_A == key ) {
+            //if (modes & GLFW_MOD_CONTROL)
+            // move left along -X
+            mat = glm::translate(glm::mat4(1.0f), glm::vec3(transStep, 0.0f, 0.0f));
+            matView = mat * matView;
+        } else if (GLFW_KEY_D == key) {
+            // move right along X
+            mat = glm::translate(glm::mat4(1.0f), glm::vec3(-transStep, 0.0f, 0.0f));
+            matView = mat * matView;
+        } if (GLFW_KEY_W == key ) {
+            // move forward along -Z
+            mat = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, transStep));
+            matView = mat * matView;
+        } else if (GLFW_KEY_S == key) {
+            // move backward along Z
+            mat = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -transStep));
+            matView = mat * matView;
+        } 
+
+        // translation along world axis
+        else if (GLFW_KEY_H == key ) {
+            //if (modes & GLFW_MOD_CONTROL)
+            // move left along -X
+            mat = glm::translate(glm::mat4(1.0f), glm::vec3(transStep, 0.0f, 0.0f));
+            matView = matView * mat;
+        } else if (GLFW_KEY_L == key) {
+            // move right along X
+            mat = glm::translate(glm::mat4(1.0f), glm::vec3(-transStep, 0.0f, 0.0f));
+            matView = matView * mat;
+        } if (GLFW_KEY_J == key ) {
+            // move forward along Z
+            mat = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -transStep));
+            matView = matView * mat;
+        } else if (GLFW_KEY_K == key) {
+            // move backward along -Z
+            mat = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, transStep));
+            matView = matView * mat;
+        } 
+    }
+    
+}
+
+int main()
+{
+    GLFWwindow *window;
+
+    // GLFW init
+    if (!glfwInit())
+    {
+        std::cout << "glfw failed" << std::endl;
+        return -1;
+    }
+
+    // create a GLFW window
+    window = glfwCreateWindow(wView, hView, "Hello Skeletal Animation", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    // register the key event callback function
+    glfwSetKeyCallback(window, key_callback);
+
+    glfwSetWindowSizeCallback(window, window_size_callback);
+
+
+    // loading glad
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Couldn't load opengl" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+
+    blinnShader = initShader( "shaders/blinn.vert", "shaders/blinn.frag");
+    setLightPosition(lightPos);
+    setViewPosition(viewPos);
+
+    texblinnShader = initShader("shaders/texblinn.vert", "shaders/texblinn.frag");
+    setLightPosition(lightPos);
+    setViewPosition(viewPos);
+
+    //normalblinnShader = initShader("shaders/normalblinn2.vert", "shaders/normalblinn2.frag");
+    //setLightPosition(lightPos);
+    //setViewPosition(viewPos);
+
+    // set the eye at (0, 0, 5), looking at the centre of the world
+    matView = glm::lookAt(viewPos, viewCenter, glm::vec3(0, 1, 0)); 
+
+    // set the Y field of view angle to 60 degrees, width/height ratio to 1.0, and a near plane of 3.5, far plane of 6.5
+    matProj = glm::perspective(glm::radians(fov), wView / (float) hView, near, far);
+
+    std::shared_ptr<Mesh> anim_model = std::make_shared<Mesh>();
+    anim_model->init("models/vampire/dancing_vampire.dae", texblinnShader);
+
+    // anim_model->init("models/mannequin/Capoeira_Mannequin.dae", texblinnShader);
+   
+    // setting the background colour, you can change the value
+    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+    
+    glEnable(GL_DEPTH_TEST);
+
+     
+    // setting the event loop
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        anim_model->draw(matModelRoot, matView, matProj);
+        
+        glfwSwapBuffers(window);
+
+        //break;
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
